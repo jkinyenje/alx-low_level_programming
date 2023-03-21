44562e5d8206dcf23f2ac7cf0bb3f467a01b9136
jkinyenje <kinyenje@gmail.com>
2023-03-21 10:54:09 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Write a program that prints _putcharcheck the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("_putchar", \n);
+    return (0);
+}
