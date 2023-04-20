cabed605da29d5ebdf65306ce7594c9449d14cd4
jkinyenje <kinyenje@gmail.com>
2023-04-19 19:38:50 -0700

    Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - Five functions
+ * @a: input 1
+ * @b: input 2
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Five fxn
+ * @a: input 1
+ * @b: input 2
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Five fxn
+ * @a: input 1
+ * @b: input 2
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Five fxn
+ * @a: input 1
+ * @b: input 2
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Five fxn5 fxn
+ * @a: input 1
+ * @b: input 2
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
