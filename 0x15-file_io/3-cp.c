b81e292965c7fcc844c5b2492cb70f83de7f0f0a
jkinyenje <kinyenje@gmail.com>
2023-05-08 14:27:13 -0700

    input  output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,30 +1,65 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * append_text_to_file - Appends text
- * @filename: Name of the file
- * @text_content: The string to add 
- *
- * Return: NULL - -1 Otherwise - 1.
+ * @file_from: file_from
+ * @file_to: file_to
+ * @argv: arguments vector.
+ * Return: 0
  */
-
-int append_text_to_file(const char *filename, char *text_content)
+void error_file(int file_from, int file_to, char *argv[])
 {
-int o, w, len = 0;
-
-if (filename == NULL)
-	return (-1);
-if (text_content != NULL)
+if (file_from == -1)
 {
-	for (len = 0; text_content[len];)
-		len++;
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
 }
-o = open(filename, O_WRONLY | O_APPEND);
-w = write(o, text_content, len);
-if (o == -1 || w == -1)
-	return (-1);
+if (file_to == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	exit(99);
+}
+}
 
-close(o);
+int main(int argc, char *argv[])
+{
+int file_from, file_to, err_close;
+ssize_t nchars, nwr;
+char buf[1024];
 
-return (1);
+if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+file_from = open(argv[1], O_RDONLY);
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+error_file(file_from, file_to, argv);
+
+nchars = 1024;
+while (nchars == 1024)
+	{
+	nchars = read(file_from, buf, 1024);
+	if (nchars == -1)
+		error_file(-1, 0, argv);
+	nwr = write(file_to, buf, nchars);
+	if (nwr == -1)
+		error_file(0, -1, argv);
+	}
+
+err_close = close(file_from);
+if (err_close == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+	exit(100);
+}
+
+err_close = close(file_to);
+if (err_close == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+	exit(100);
+}
+	return (0);
 }
